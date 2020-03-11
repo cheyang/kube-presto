fcfc1b12b3af9736cdf3e5dea2ac43882d67ef30
Yifeng Jiang <uprushworld@gmail.com>
2020-03-11 14:06:57 +0800

    add basic jmeter benchmark

---
+++ benchmark/jmeter-bench.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+JMETER_HOME=/opt/apache-jmeter-5.2.1
+PRESTO_VER=330
+JAVA_HOME=/usr/jdk64/jdk-11.0.6
+
+# Download jdbc driver
+if [ ! -f $JMETER_HOME/lib/presto-jdbc-$PRESTO_VER.jar ]; then
+	curl -o $JMETER_HOME/lib/presto-jdbc-$PRESTO_VER.jar https://repo1.maven.org/maven2/io/prestosql/presto-jdbc/$PRESTO_VER/presto-jdbc-$PRESTO_VER.jar
+fi
+
+# Run jmeter CLI
+REPORT_DIR="report/`date +%s`"
+mkdir -p $REPORT_DIR
+mkdir -p logs
+
+JAVA_HOME=$JAVA_HOME $JMETER_HOME/bin/jmeter \
+	-n -t jmeter-tpcds.jmx \
+	-l logs/tpcds-log.jtl \
+	-e -o reports $REPORT_DIR
