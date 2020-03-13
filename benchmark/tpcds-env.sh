7f7abf9c521c304d2a9eab4fcac71334326db007
Yifeng Jiang <uprushworld@gmail.com>
2020-03-13 09:47:43 +0000

    update env

--- benchmark/tpcds-env.sh
+++ benchmark/tpcds-env.sh
@@ -3,4 +3,4 @@
 SCALE=1000
 FORMAT=orc
 SCHEMA=hs_tpcds_sf${SCALE}_${FORMAT}
-LOCATION=s3://bigdatabucket/warehouse/tpcds_sf${SCALE}_${FORMAT}.db
+LOCATION=s3a://bigdatabucket/warehouse/tpcds_sf${SCALE}_${FORMAT}.db
