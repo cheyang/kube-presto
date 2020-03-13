952b4b7f0da55b2597eb5308a459866cd869211e
Yifeng Jiang <uprushworld@gmail.com>
2020-03-13 15:38:22 +0800

    replace schema using variable

--- benchmark/tpcds-sql.sh
+++ benchmark/tpcds-sql.sh
@@ -4,6 +4,7 @@
 #
 
 source ./tpcds-env.sh
+echo $SCHEMA
 
 PRESTO_CODE=~/code/prestosql
 TPCDS_SQL=$PRESTO_CODE/presto-benchto-benchmarks/src/main/resources/sql/presto/tpcds
@@ -13,7 +14,7 @@ mkdir tpcds
 for SQL in $TPCDS_SQL/*.sql
 do
 	FILENAME=`basename $SQL`
-	cat $SQL | sed 's/${database}/hive/g' | sed 's/${schema}/${SCHEMA}/g' > tpcds/$FILENAME
+	cat $SQL | sed 's/${database}/hive/g' | sed "s/\${schema}/${SCHEMA}/g" > tpcds/$FILENAME
 done
 
 echo "Replaced SQL to ./tpcds"
