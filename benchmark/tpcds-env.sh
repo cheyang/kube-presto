dc2cd293273a28059242c245edd291b2447d8073
Yifeng Jiang <uprushworld@gmail.com>
2020-03-13 15:40:38 +0800

    use hyperstore

--- benchmark/tpcds-env.sh
+++ benchmark/tpcds-env.sh
@@ -2,5 +2,5 @@
 
 SCALE=1000
 FORMAT=orc
-SCHEMA=tpcds_sf${SCALE}_${FORMAT}
-LOCATION=s3://deephub/mydir/$SCHEMA.db
+SCHEMA=hs_tpcds_sf${SCALE}_${FORMAT}
+LOCATION=s3://bigdatabucket/warehouse/tpcds_sf${SCALE}_${FORMAT}.db
